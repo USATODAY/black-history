decb154abf251a99201b79f7d978ea41af807b63
mitchthorson <mthorson@usatoday.com>
2015-02-05 14:24:56 -0500

    get filter filtering to layout and transition with isotope

--- src/js/views/TagsView.js
+++ src/js/views/TagsView.js
@@ -8,7 +8,7 @@ define(
   function(jQuery, _, Backbone, TagView) {
     return Backbone.View.extend({
         initialize: function() {
-            
+           this.listenTo(this.collection, 'change:isActive', this.filter);
         },
         events: {
             "click .tags-next-button": "onNextClick"
@@ -16,6 +16,10 @@ define(
         className: 'iapp-panel upcoming',
         template: templates['tags.html'],
         render: function(data) {
+
+            //temp remove once config is in place
+            var config = { isMobile: false};
+
             var _this = this;
             this.$el.html(this.template());
             
@@ -23,11 +27,23 @@ define(
                  var tagView = new TagView({model: tagModel})
                  _this.$('.iapp-tag-container').append(tagView.render().el);
             });
+
+            _.defer(function() {
+                    this.$('.iapp-tag-container').isotope({
+                        itemSelector: '.iapp-tag-item',
+                        transitionDuration: (!config.isMobile) ? '0.4s' : 0,
+                        layoutMode: 'fitRows'
+
+                });
+            });
             
             return this;
         },
         onNextClick: function() {
             Backbone.trigger("app:advance");
+        },
+        filter: function() {
+            this.$('.iapp-tag-container').isotope({filter: ':not(.unavailable)'});
         }
     });
 
