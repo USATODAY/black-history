e1e5983cb6c261563c9bbcf8729a52c80cc6a703
mitchthorson <mthorson@usatoday.com>
2015-02-09 16:10:18 -0500

    add share popup functionality

--- src/js/views/ShareView.js
+++ src/js/views/ShareView.js
@@ -16,13 +16,32 @@ define(
                 return this;
             },
             events: {
-                'click .iapp-share-close': 'onClickClose'
+                'click .iapp-share-close': 'onClickClose',
+                'click .iapp-share-popup': 'onShareButtonClick'
             },
             template: templates['share.html'],
             className: 'iapp-share-panel iapp-panel upcoming',
             onClickClose: function() {
                 this.$el.addClass('upcoming').removeClass('active');
                 Backbone.trigger('share:close');
+            },
+            onShareButtonClick: function(e) {
+                e.preventDefault();
+                
+
+              this.windowPopup(e.currentTarget.href, 500, 300);
+            },
+            windowPopup: function(url, width, height) {
+                // Calculate the position of the popup so
+                // it’s centered on the screen.
+                var left = (screen.width / 2) - (width / 2),
+                top = (screen.height / 2) - (height / 2);
+
+                window.open(
+                    url,
+                    "",
+                    "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+                );
             }
         });
 });
